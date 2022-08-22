bea336528bd7366133402ddf098f6a7d9be1c40a
Anton <altay77@inbox.ru>
2022-08-22 23:38:28 +0700

    TaskToo solution

---
+++ TaskOne/TaskToo/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача два : принимает на вход три числа и выдаёт максимальное из этих чисел
+Console.Write("Введите первое целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье целое число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int big = num1;
+if (num1 < num2)
+{
+    big = num2;
+}
+if (big < num3)
+{
+    big = num3;
+}
+Console.WriteLine("Наибольшее число: "+big);
