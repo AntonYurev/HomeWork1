011d51a7494149761caf3af8990cc1fded3f0d36
Anton <altay77@inbox.ru>
2022-08-22 22:35:46 +0700

    TaskOne Solution

--- TaskOne/Program.cs
+++ TaskOne/Program.cs
@@ -1,2 +1,14 @@
 ﻿// Задача один : Ввод двух чисел, вывод какое больше
-Console.WriteLine("Hello, World!");
+Console.Write("Введите первое целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+else
+{
+    Console.WriteLine("Второе число больше первого");
+}
+ 
