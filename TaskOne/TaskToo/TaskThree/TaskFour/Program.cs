30fc40421bf3d5071054ef1d96ee21305242c495
Anton <altay77@inbox.ru>
2022-08-23 23:21:57 +0700

    taskfour Solution

---
+++ TaskOne/TaskToo/TaskThree/TaskFour/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача четыре: на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+if (num1 < 0)
+{
+    a = -1;
+}
+int num = 2;
+while (num <= num1*a)
+{
+if (num%2 == 0)
+{
+    Console.Write(num*a + ", ");
+}
+num = num + 1;
+} 
